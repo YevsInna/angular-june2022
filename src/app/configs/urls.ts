04aabcd2641fc438a728692f153d21b849dfcfcc
YevsInna <evseinka@gmail.com>
2022-10-25 13:13:05 +0200

    lesson 1

---
+++ src/app/configs/urls.ts
@@ -0,0 +1,8 @@
+const baseURL = 'https://jsonplaceholder.typicode.com';
+
+const urls = {
+  users: `${baseURL}/users`,
+  posts: `${baseURL}/posts`,
+  comments: `${baseURL}/comments`
+}
+export {urls};
---
+++ src/app/services/post.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+import {IPost} from "../interfaces";
+import {urls} from "../configs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAll(): Observable<IPost[]>{
+    return this.httpClient.get<IPost[]>(urls.posts)
+  }
+}
