a49b29d4e0c94b94b8324cb71e21b39e9e1662a2
YevsInna <evseinka@gmail.com>
2022-10-27 17:45:54 +0200

    lesson 2 routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { UsersComponent } from './components/users/users.component';
-import { UserComponent } from './components/user/user.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { PostComponent } from './components/post/post.component';
-import { CommentsComponent } from './components/comments/comments.component';
-import { CommentComponent } from './components/comment/comment.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+
+import {AppComponent} from './app.component';
+import {UsersComponent} from './components/users/users.component';
+import {UserComponent} from './components/user/user.component';
+import {PostsComponent} from './components/posts/posts.component';
+import {PostComponent} from './components/post/post.component';
+import {CommentsComponent} from './components/comments/comments.component';
+import {CommentComponent} from './components/comment/comment.component';
+import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
+import {HeaderComponent} from './components/header/header.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: 'users', pathMatch: 'full'},
+      {path: 'users', component: UsersComponent, children:[
+          {path: ':id', component: UserDetailsComponent}
+        ]},
+      {path: 'posts', component: PostsComponent},
+      {path: 'comments', component: CommentsComponent}
+    ]
+  }
+];
 
 @NgModule({
   declarations: [
@@ -18,13 +35,18 @@ import {HttpClientModule} from "@angular/common/http";
     PostsComponent,
     PostComponent,
     CommentsComponent,
-    CommentComponent
+    CommentComponent,
+    MainLayoutComponent,
+    HeaderComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -11,6 +11,8 @@ import {UserService} from "../../services/user.service";
 export class UsersComponent implements OnInit {
   users: IUser[];
 
+  selectedUser: IUser;
+
   constructor(private userService: UserService) {
   }
 
@@ -18,4 +20,7 @@ export class UsersComponent implements OnInit {
     this.userService.getAll().subscribe(value => this.users = value)
   }
 
+  getUser(user: IUser) {
+    this.selectedUser = user
+  }
 }
